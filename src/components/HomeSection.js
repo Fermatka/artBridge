19b225df01e3c8d08ab07a65377a7a424c1d865a
Fermatka <agatka.fermatka@gmail.com>
2024-08-09 09:12:21 +0200

    Artbridge test text added

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,10 +1,13 @@
+import Artbridge from "./shared/Artbridge";
+
 export default function HomeSection() {
   return (
     <section
       id="home"
-      className="min-h-screen bg-gray-200 flex flex-col justify-center text-center align-middle"
+      className="min-h-screen flex flex-col justify-center text-center align-middle"
     >
-      <h1 className="text-center h1">ARTBRIDGE</h1>
+      <Artbridge />
+
       <div className="h4 flex text-center justify-center">
         gdzie sztuka łączy ludzi
       </div>
---
+++ src/components/shared/Artbridge.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Artbridge = () => {
+  return (
+    <div className="relative flex items-center justify-center bg-gray-100">
+      <h1 className="text-[12rem] font-bold text-transparent bg-clip-text bg-[url('/pink-concert.jpg')] bg-cover">
+        ARTBRIDGE
+      </h1>
+    </div>
+  );
+};
+
+export default Artbridge;
